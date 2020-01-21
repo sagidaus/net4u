425925c2ed3457f8e0ea546bb6ebd9d2321e9944
sagidaus <sagidaos1@gmail.com>
2020-01-21 16:35:57 +0200

    lotto project 21.1.20

--- lotto/lotto.py
+++ lotto/lotto.py
@@ -6,7 +6,7 @@ def menu():
 
     while (exit == "yes"):
         print("Lotto menu: \n1. Manual\n2. Auto\n3. Double \n4. Check winning")
-        win = random.sample(range(1, 10), 6)
+        win = random.sample(range(1, 37), 6)
         choose = int(input("Choose number: "))
         if choose == 1:
             results = manual()
@@ -14,8 +14,8 @@ def menu():
         elif choose == 2:
             results = auto()
             calculate_win(results, win)
-        elif choose == 3:
-            print("3")
+        # elif choose == 3:
+        #     print("3")
         # elif choose == 4:
         #     check_win(manu)
         else:
@@ -27,12 +27,9 @@ def menu():
         else:
             print("Bye bye")
             break
-    print("winnig numbers: " + str(win))
-
 
 def get_list_from_user():
     results = []
-
     print("Enter your 6 number in a row: ")
     for j in range(6):
         num = int(input("Enter number: "))
@@ -41,7 +38,6 @@ def get_list_from_user():
             num = int(input("Enter number: "))
         results.append(num)
     return results
-
 
 def manual():
     money = int(input("Enter how many shekels you have: "))
@@ -60,37 +56,28 @@ def manual():
         print("list #%s: %s: " % (i, current_results))
     return customer_lists
 
-
 def auto():
     money = int(input("Enter how many shekels you have: "))
     if money < ONE_LIST_PRICE:
         print("minimum %s shekels" % ONE_LIST_PRICE)
     num_of_lists = money // ONE_LIST_PRICE
     print("Please enter %s list" % num_of_lists)
-
-    # print("You get " + str(int(money // 3)) + " auto list's")
     autolist = []
     for i in range(num_of_lists):
-        auto = random.sample(range(1, 10), 6)
+        auto = random.sample(range(1, 37), 6)
         autolist.append(auto)
-    # print(autolist)
     return (autolist)
-    # check_win(autolist)
-
 
 def double():
     print("double")
 
-
 # get a list in a and return the total prize
-def check_win(qqq, win):
+def check_win(a_list, win):
     guesses = 0
-
     # count the number of right guesses
-    for num in qqq:
+    for num in a_list:
         if num in win:
             guesses += 1
-
     # calculate the reward
     reward = 0
     if guesses == 6:
@@ -101,9 +88,7 @@ def check_win(qqq, win):
         reward = 250
     elif guesses == 3:
         reward = 10
-
     return reward
-
 
 def calculate_win(lists, win):
     total_reward = 0
@@ -116,5 +101,4 @@ def calculate_win(lists, win):
 
 ################### main code ##############################
 
-### winnig list ###
 print(menu())
